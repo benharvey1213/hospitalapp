d69311ddc4c0f62f7c1ae021fe05bde2d65be2bc
Ben Harvey <ben.harvey1213@gmail.com>
2019-12-10 12:55:21 -0600

    Updates

--- HospitalApp/Default.aspx
+++ HospitalApp/Default.aspx
@@ -5,7 +5,9 @@
     <div class="jumbotron">
         <h1>Hospital</h1>
         <p>&nbsp;</p>
-        <p><a href="/MyStuff/Dashboard.aspx" class="btn btn-primary btn-lg">View Dashboard</a>&nbsp; </p>
+        <p id="dashp" runat="server">&nbsp;<asp:Button ID="DashboardButton" runat="server" Text="Dashboard" OnClick="Button1_Click" Width="165px" /></p>
+        <p id="inp" runat="server">&nbsp;<asp:Button ID="LoginButton" runat="server" Text="Login" OnClick="Button2_Click" Width="165px" /></p>
+        <p id="outp" runat="server">&nbsp;<asp:Button ID="LogoutButton" runat="server" Text="Logout" OnClick="Button3_Click" Width="165px"/></p>
     </div>
 
     <div class="row">
--- HospitalApp/Default.aspx.cs
+++ HospitalApp/Default.aspx.cs
@@ -11,7 +11,44 @@ namespace HospitalApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string username = null;
+            string name = null;
+            try
+            {
+                username = Request.Cookies["username"].Value;
+                name = Request.Cookies["name"].Value;
+                
+                // LoginButton.Visible = false;
+                inp.Visible = false;
+            }
+            catch
+            {
+                // DashboardButton.Visible = false;
+                // LogoutButton.Visible = false;
 
+                dashp.Visible = false;
+                outp.Visible = false;
+            }
+        }
+
+        // for dashboard
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("/MyStuff/Dashboard.aspx");
+        }
+
+        // login
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("MyStuff/Login.aspx");
+        }
+
+        // logout
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Response.Cookies["name"].Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies["username"].Expires = DateTime.Now.AddDays(-1);
+            Response.Redirect("/Default.aspx");
         }
     }
 }
