29be19955aea70c4fecef40149e03fde9a90ec63
Ben Harvey <ben.harvey1213@gmail.com>
2019-12-18 11:29:01 -0600

    commits

--- HospitalApp/MyStuff/Doctor.aspx
+++ HospitalApp/MyStuff/Doctor.aspx
@@ -9,12 +9,13 @@
     <p>&nbsp;</p>
 
     <p>
-        <asp:Button ID="Button1" runat="server" CssClass="btn" Text="Manage Appointments" Width="293px" OnClick="Button1_Click" style="font-size: large" />
+        <asp:Button ID="Button2" runat="server" class="btn" OnClick="Button2_Click" Text="View Patients" Width="293px" style="font-size: large" />
     </p>
 
     <p>
-        <asp:Button ID="Button2" runat="server" class="btn" OnClick="Button2_Click" Text="View Patients" Width="293px" style="font-size: large" />
+        <asp:Button ID="Button1" runat="server" CssClass="btn" Text="Manage Appointments" Width="293px" OnClick="Button1_Click" style="font-size: large" />
     </p>
+
 
     <p>
         <asp:Button ID="Button3" runat="server" class="btn" Text="Messages" Width="293px" OnClick="Button3_Click" style="font-size: large" />
--- HospitalApp/MyStuff/Doctor.aspx.designer.cs
+++ HospitalApp/MyStuff/Doctor.aspx.designer.cs
@@ -24,15 +24,6 @@ namespace HospitalApp.MyStuff
         protected global::System.Web.UI.WebControls.Label GreetingLabel;
 
         /// <summary>
-        /// Button1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
-
-        /// <summary>
         /// Button2 control.
         /// </summary>
         /// <remarks>
@@ -40,6 +31,15 @@ namespace HospitalApp.MyStuff
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button2;
+
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
 
         /// <summary>
         /// Button3 control.
